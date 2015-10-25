e327613cf4c05422fdf838089f7dcf9cd8228e37
onehp <onehitpoint@googlemail.com>
2015-10-25 01:40:25 +0100

    Filter active tags

--- src/main/java/com/maddogs/repo/TagRepository.java
+++ src/main/java/com/maddogs/repo/TagRepository.java
@@ -1,11 +1,17 @@
 package com.maddogs.repo;
 
 import com.maddogs.domain.Tag;
-import com.maddogs.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "tag", path = "tag", excerptProjection = TagProjection.class)
 public interface TagRepository extends CrudRepository<Tag,Long> {
+
+    @Query("SELECT t FROM Tag t WHERE t.expireDateTime IS NULL OR t.expireDateTime < CURRENT_TIME")
+    List<Tag> active();
 
 }
