912dd5a47b8ef7dd1237d0824aff0ec2ad09b96d
onehp <onehitpoint@googlemail.com>
2015-10-25 09:17:00 +0000

    More CORS

--- src/main/java/com/maddogs/config/SimpleCORSFilter.java
+++ src/main/java/com/maddogs/config/SimpleCORSFilter.java
@@ -17,6 +17,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		response.setHeader("Access-Control-Expose-Headers", "Location, X-Location");
 		chain.doFilter(req, res);
 	}
 
