7162f6f1fa35e05d15a01a2184c8cd44cc37f7ca
onehp <onehitpoint@googlemail.com>
2015-10-25 10:40:42 +0000

    Add image to social tag

--- src/main/java/com/maddogs/repo/TagRepository.java
+++ src/main/java/com/maddogs/repo/TagRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "tag", path = "tag", excerptProjection = TagProjection.class)
